0bf8e2ac04b8a867cd247ed5b1ebcab5915f345b
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2016-08-25 10:58:15 +0000

    fixes auto versioning

--- scripts/version.js
+++ scripts/version.js
@@ -1,5 +1,7 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
+var fs = require('fs');
+var path = require('path');
 
 var diff = shell.exec('git diff --exit-code package.json');
 var output = diff.stdout;
@@ -7,5 +9,24 @@ var versionChanged = diff.code == 1 && /-\s*\"version\"/gim.test(output) && /\+\
 
 if(!versionChanged) {
   console.log('Version hasn\'t been changed. Patching');
-  shell.exec('npm version patch --force');
+  var filePath = path.join(__dirname, '../package.json');
+  fs.readFile(filePath, { encoding: 'utf-8' }, function(err, data) {
+    if(!err) {
+      var packageJson = JSON.parse(data);
+      console.log('Old version: ', packageJson.version);
+      var split = packageJson.version.split('.');
+      split[2]++;
+      var version = split.join('.');
+      packageJson.version = version;
+      console.log('New version: ', version);
+      fs.writeFile(filePath, JSON.stringify(packageJson, null, 2), function(err) {
+        if(err) {
+          console.log(err);
+        }
+        else {
+          console.log('Version updated.')
+        }
+      })
+    }
+  })
 }
