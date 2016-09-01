c331c92054c7593b05c4b4f2a414481ff4c3ef9c
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2016-09-01 14:24:04 +0000

    changes project structure
    trying to get symlink to work in a good way

--- index.ts
+++ index.ts
@@ -1,39 +1,43 @@
 import { BrowserModule  } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
-
 import { ModuleWithProviders, provide, NgModule } from '@angular/core';
 
 import { ApiSwaggerComponent } from './components/api-swagger/api-swagger.component';
+export { ApiSwaggerComponent } from './components/api-swagger/api-swagger.component';
 import { ApiMethodComponent } from './components/api-method/api-method.component';
+export { ApiMethodComponent } from './components/api-method/api-method.component';
 
 import { SwaggerService } from './services/swagger.service';
+export { SwaggerService } from './services/swagger.service';
 
 import { KeyValuePairsPipe } from './pipes/key-value-pairs.pipe';
+export { KeyValuePairsPipe } from './pipes/key-value-pairs.pipe';
 
 export * from './services/template.provider';
-
+export * from './schema/2.0/swagger.schema';
+import {Swagger} from './schema/2.0/swagger.schema';
 
 export const SWANGULAR_COMPONENTS: any[] = [
   ApiSwaggerComponent,
-  ApiMethodComponent,
-  KeyValuePairsPipe
+  ApiMethodComponent
 ];
 
 export const SWANGULAR_PROVIDERS: any[] = [
   SwaggerService
 ];
-
 @NgModule({
-    declarations: SWANGULAR_COMPONENTS,
-    exports: SWANGULAR_COMPONENTS,
+    declarations: [
+      ...SWANGULAR_COMPONENTS,
+      KeyValuePairsPipe
+    ],
+    exports: [
+      ...SWANGULAR_COMPONENTS
+    ],
     imports: [
       BrowserModule,
       HttpModule
     ]
 })
-
-
-
 export class SwangularModule {
   static forRoot(): ModuleWithProviders {
     return {
@@ -42,9 +46,3 @@ export class SwangularModule {
     };
   }
 }
-// export * from './components/api-swagger/api-swagger.component';
-// export * from './components/api-method/api-method.component';
-// export * from './services/swagger.service';
-// export * from './schema/2.0/swagger.schema';
-//
-// export * from './swangular.module';
